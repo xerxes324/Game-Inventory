74017041e8fa9e66cdd134ad3edec60d576b4627
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-30 19:43:49 +0530

    initial express setup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./routes/userRouter");
 const app = express();
-const path = require("node:path");
-
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+const PORT = process.env.port || 3000;
+
+app.use("/", userRouter);
+
+app.listen(PORT, ()=>{
+    console.log(`Starting server on port ${PORT}`);
+})
