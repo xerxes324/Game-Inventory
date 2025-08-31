aa9bf492646ee624022d4d31ae946e6d417a46a4
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-31 19:05:09 +0530

    feat : added 4 more routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,11 @@ const userRouter = Router();
 const userController = require("../controllers/userController");
 
 userRouter.get("/", userController.homepage);
+userRouter.get("/searchGame", userController.searchGame);
+userRouter.get("/tableList", userController.viewTable);
+userRouter.get("/games",userController.showGames);
+userRouter.get("/developers", userController.showDevelopers);
+userRouter.get("/genres", userController.showGenres);
 
 module.exports = userRouter;
 
