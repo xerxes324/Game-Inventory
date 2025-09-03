fd6e796254dcf13513eb8d3615c0c3246f1c9d0b
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-09-04 01:00:20 +0530

    added express.json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = require("./routes/userRouter");
 const app = express();
+app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 
