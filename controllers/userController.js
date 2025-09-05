6d6634dcee76a9bfe755d27102ba13d4fda6d638
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-09-05 23:23:57 +0530

    minor code fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,14 +11,12 @@ exports.searchGame = async(req,res)=>{
     if ( req.query.search.length > 0 ){
         const game = req.query.search;
         const displayCheck = await db.displayGameInfo(game);
-        // res.send(displayCheck);
-        console.log(displayCheck.length);
 
         if ( displayCheck.length > 0 ){
             res.render("searchResults",{dc : displayCheck});
         }
         else{
-            res.send("Game Not Found")
+            res.render("error");
         }
         
     }
@@ -26,26 +24,21 @@ exports.searchGame = async(req,res)=>{
 
 exports.viewTable = async(req,res) => {
     const content = await db.inventory();
-    // console.log(content.length, "is the length");
-    // console.log(content, "is the content");
     res.render("table", {content : content});
 }
 
 exports.showGames = async(req,res)=>{
     const games = await db.getGames();
-    console.log(games, 'is the games');
     res.render("games", {gamename : games});
 }
 
 exports.showDevelopers = async(req,res) => {
     const devs = await db.getDevs();
-    console.log(devs, 'is the dev');
     res.render("developers", {dev : devs});
 }
 
 exports.showGenres = async(req,res) => {
     const gen = await db.getGenres();
-    console.log(gen);
     res.render("genres", {genre: gen});
 }
 
@@ -54,12 +47,9 @@ exports.edit = async(req,res) => {
     const dev = req.query.devName;
     const gen = req.query.genreName;
     const id = req.query.id;
-    // console.log(game, dev, gen, id , "are the four.");  
-    
+
     await db.editRow(game,dev,gen,id);
-    const content = await db.inventory();
-    console.log(content,"is the content");
-    res.render("table", {content: content});
+    res.redirect("/tableList");
 }
 
 exports.addGame = async(req,res) => {
@@ -67,15 +57,11 @@ exports.addGame = async(req,res) => {
     const dev = req.query.devName;
     const gen = req.query.genName;
     await db.addGame(game,dev,gen);
-
-    const content = await db.inventory();
-    res.render("table", {content: content});
+    res.redirect("/tableList");
 }
 
 exports.deleteGame = async(req,res) => {
     const id = req.body.id;
     await db.deleteRow(id);
-    const content = await db.inventory();
-    console.log(content, "is the content");
-    res.render("table", {content: content});
+    res.redirect("/tableList");
 }
